8b7fb1974d703012d2a2d97130524b56b48a76d1
Colin J. Kautz <colin.kautz@gmail.com>
2020-04-04 18:10:42 -0500

    adding playedmyself command

---
+++ src/commands/Fun/playedmyself.ts
@@ -0,0 +1,20 @@
+import { SteveCommand } from '@lib/structures/commands/SteveCommand';
+import { CommandStore, KlasaMessage } from 'klasa';
+import { Message } from 'discord.js';
+
+export default class extends SteveCommand {
+
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			cooldown: 60,
+			cooldownLevel: 'channel',
+			aliases: ['i played myself'],
+			description: 'congratulations, you played yourself.',
+			extendedHelp: 'this command returns the DJ Khaled GIF'
+		});
+	}
+
+	async run(msg: KlasaMessage): Promise<Message> {
+		return msg.channel.send({ files: [{ attachment: './assets/images/played_yourself.gif', name: 'played_yourself.gif' }] });
+	}
+}
