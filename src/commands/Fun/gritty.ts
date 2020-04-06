24bab4207166e0582f9cbdc9fb52e30a78708e33
Jonathan <jonathanwford156@gmail.com>
2020-04-06 16:01:07 -0400

    feat: add gritty command

---
+++ src/commands/Fun/gritty.ts
@@ -0,0 +1,21 @@
+import { SteveCommand } from '@lib/structures/commands/SteveCommand';
+import { CommandStore, KlasaMessage } from 'klasa';
+import { Message } from 'discord.js';
+
+export default class extends SteveCommand {
+
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			description: 'Post a random Gritty GIF.',
+			examples: ['gritty'],
+			extendedHelp: 'Gritty is our lord and savior. (He\'s actually the mascot for Philadelphia\'s hockey team but shhh.)'
+		});
+	}
+
+	public run(msg: KlasaMessage): Promise<Message> {
+		const num = Math.floor(Math.random() * 5) + 1;
+		console.log(num);
+		return msg.channel.send({ files: [{ attachment: `./assets/images/gritty/gritty_${num}.gif` }] });
+	}
+
+}
