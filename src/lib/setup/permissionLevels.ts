193fe12d949ac4ce817ae2481f17d0110f5b7883
Jonathan <jonathanwford156@gmail.com>
2020-04-08 13:30:48 -0400

    build: fix compile-time errors in permissionLevels

--- src/lib/setup/permissionLevels.ts
+++ src/lib/setup/permissionLevels.ts
@@ -6,18 +6,18 @@ import { Permissions } from 'discord.js';
 export default Client.defaultPermissionLevels = new PermissionLevels()
 	.add(Levels.EVERYONE, () => true)
 	.add(Levels.TRUSTED, msg => msg.member
-		? msg.guild.settings.get(GuildSettings.Roles.Trusted)
-			? msg.member.roles.cache.has(msg.guild.settings.get(GuildSettings.Roles.Trusted))
+		? msg.guild!.settings.get(GuildSettings.Roles.Trusted)
+			? msg.member.roles.cache.has(msg.guild!.settings.get(GuildSettings.Roles.Trusted))
 			: false
 		: false, { fetch: true })
 	.add(Levels.MODERATOR, msg => msg.member
-		? msg.guild.settings.get(GuildSettings.Roles.Moderator)
-			? msg.member.roles.cache.has(msg.guild.settings.get(GuildSettings.Roles.Moderator))
+		? msg.guild!.settings.get(GuildSettings.Roles.Moderator)
+			? msg.member.roles.cache.has(msg.guild!.settings.get(GuildSettings.Roles.Moderator))
 			: msg.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)
 		: false, { fetch: true })
 	.add(Levels.ADMINISTRATOR, msg => msg.member
-		? msg.guild.settings.get(GuildSettings.Roles.Administrator)
-			? msg.member.roles.cache.has(msg.guild.settings.get(GuildSettings.Roles.Administrator))
+		? msg.guild!.settings.get(GuildSettings.Roles.Administrator)
+			? msg.member.roles.cache.has(msg.guild!.settings.get(GuildSettings.Roles.Administrator))
 			: msg.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)
 		: false, { fetch: true })
 	.add(Levels.OWNER, ({ author, client }) => client.owners.has(author));
