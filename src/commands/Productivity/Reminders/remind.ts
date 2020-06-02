f73149bf2034d7523e49df03c4533e074d95a26f
Jonathan <jonathanwford156@gmail.com>
2020-06-01 23:04:00 -0400

    I'M SORRY SARAH

--- src/commands/Productivity/Reminders/remind.ts
+++ src/commands/Productivity/Reminders/remind.ts
@@ -18,10 +18,10 @@ export default class extends SteveCommand {
 	}
 
 	public async run(msg: KlasaMessage, [reminder, duration]: [string, number]): Promise<Message> {
-		const userReminders = await this.client.schedule.getUserReminders(msg.author.id);
-		if (userReminders.length > 25) {
-			throw 'There\'s a maximum of 25 reminders... and you\'ve got 25.';
-		}
+		// const userReminders = await this.client.schedule.getUserReminders(msg.author.id);
+		// if (userReminders.length > 25) {
+		// 	throw 'There\'s a maximum of 25 reminders... and you\'ve got 25.';
+		// }
 
 		const reminderChannel = msg.guild ? msg.guild.settings.get(GuildSettings.Channels.ReminderChannel) : null;
 
