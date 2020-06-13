f7070eb0841bf12e2d80f7f9b823a31a595587ac
Tom Wilkinson <tom@tawilkinson.com>
2020-06-13 01:31:02 +0100

    Updated dice roller to support modifiers and also chaned result formatting

--- src/commands/Fun/Random/roll.ts
+++ src/commands/Fun/Random/roll.ts
@@ -3,14 +3,18 @@ import { CommandStore, KlasaMessage } from 'klasa';
 import { Message } from 'discord.js';
 import { oneLine } from 'common-tags';
 
-const DICE_REGEX = /(?<count>\d{1,2})?d(?<sides>\d{1,4})(?<explode>!)?(?<keep>kl?(?<keepCount>\d{1,2}))?/;
+const DICE_REGEX = /(?<count>\d{1,2})?d(?<sides>\d{1,4})(?<explode>!)?(?<keep>kl?(?<keepCount>\d{1,2}))?((?<plus>\+)?(?<minus>-)?(?<mod>\d{1,2})|$)/;
 
 type KeepType = 'highest' | 'lowest' | false;
+
+type ModType = '+' | '-' | '';
 
 interface RollSpec {
 	input: string;
 	count: number;
 	sides: number;
+	operator: ModType;
+	mod: number;
 	explodes: boolean;
 	keep: KeepType;
 	keepCount?: number;
@@ -19,6 +23,7 @@ interface RollSpec {
 interface DiceResult {
 	spec: RollSpec;
 	rolls: number[];
+	message: string;
 }
 
 export default class extends SteveCommand {
@@ -48,7 +53,16 @@ export default class extends SteveCommand {
 			let sides = parseInt(match.groups.sides, 10);
 			if (sides > 1000) sides = 1000;
 
+			let mod = parseInt(match.groups.mod, 10);
+			if (mod > 99) mod = 99;
+
+			let operator: ModType = '';
 			const explodes = match.groups.explode === '!';
+			if (match.groups.minus === '-') {
+				operator = '-';
+			} else if (match.groups.plus === '+') {
+				operator = '+';
+			}
 
 			let keep: KeepType = false;
 			if (match.groups.keep) {
@@ -59,7 +73,7 @@ export default class extends SteveCommand {
 
 			const keepCount = parseInt(match.groups.keepCount, 10);
 
-			return { input: match.input, count, sides, explodes, keep, keepCount };
+			return { input: match.input, count, sides, operator, mod, explodes, keep, keepCount };
 		});
 	}
 
@@ -68,18 +82,54 @@ export default class extends SteveCommand {
 
 		for (const spec of specs) {
 			let rolls = [];
+			let lost = [];
+			let message = '';
+			let operator = '';
+
+			if (spec.operator === '+') { operator = '+'; }
+			const shift = this.getMod(spec.operator, spec.mod);
+
 			for (let i = 0; i < spec.count; i++) {
 				const roll = this.roll(spec.sides, spec.explodes);
 				rolls.push(roll);
 			}
 
+			const len = rolls.length;
+
 			if (spec.keep) {
 				rolls.sort();
 				if (spec.keep === 'highest') rolls.reverse();
+				lost = rolls.slice(spec.keepCount);
 				rolls = rolls.slice(0, spec.keepCount);
 			}
 
-			results.push({ spec, rolls });
+			if (len === 1) {
+				const roll = rolls[0];
+				message = `**${roll}**`;
+				if (shift !== 0) {
+					const sum = roll + shift;
+					message = `_${message} ${operator} ${shift}_ = **${sum}**`;
+				}
+			} else {
+				let sum = 0;
+				for (const roll of rolls) {
+					sum += roll;
+				}
+				if (lost.length !== 0) {
+					message = `(~~${lost.join(',')}~~ ${rolls.join('+')})`;
+				} else {
+					message = `(${rolls.join('+')})`;
+				}
+				if (shift !== 0) {
+					sum += shift;
+					message = `_${message} ${operator} ${shift}_`;
+				} else {
+					message = `_${message}_`;
+				}
+				message += ` = ** ${sum}**`;
+			}
+
+			results.push({ spec, rolls, message });
 		}
 
 		const getEmoji = (spec): string => {
@@ -92,13 +142,13 @@ export default class extends SteveCommand {
 		if (results.length === 1) {
 			const result = results[0];
 			const emoji = getEmoji(result.spec);
-			const message = `${emoji} You rolled: \`${result.rolls.join(', ')}\` ${emoji}`;
+			const message = `${emoji} You rolled: ${result.message} ${emoji}`;
 			return msg.channel.send(message);
 		} else {
 			let message = 'You rolled:';
 			for (const result of results) {
 				const emoji = getEmoji(result.spec);
-				message += `\n${emoji} ${result.spec.input}: \`${result.rolls.join(', ')}\``;
+				message += `\n${emoji} ${result.spec.input}: ${result.message}`;
 			}
 			return msg.channel.send(message);
 		}
@@ -108,12 +158,26 @@ export default class extends SteveCommand {
 		return Math.floor(Math.random() * sides) + 1;
 	}
 
+	private getMod(operator: ModType, mod: number): number {
+		let modifier = 0;
+		// minus is worse so takes precedence
+		if (operator === '-') {
+			modifier -= mod;
+		} else if (operator === '+') {
+			modifier += mod;
+		}
+		return modifier;
+	}
+
 	private roll(sides: number, explodes: boolean): number {
-		if (sides <= 1) return 1; // this one is easy
+		let total = 0;
+		let roll = 0;
+
+		if (sides <= 1) {
+			total = 1;
+		}
 
 		if (explodes) {
-			let total = 0;
-			let roll = 0;
 			do {
 				roll = this.rollOnce(sides);
 				total += roll;
@@ -121,10 +185,11 @@ export default class extends SteveCommand {
 				roll === sides
 				&& roll < 1e6 // prevent an infinite loop, just in case
 			);
-			return total;
 		} else {
-			return this.rollOnce(sides);
+			total = this.rollOnce(sides);
 		}
+
+		return total;
 	}
 
 }
