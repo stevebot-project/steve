1a032d262547450675e28faa4d2a019c84ee2c06
BenSegal855 <bensegal@udel.edu>
2020-06-18 17:42:30 -0400

    Cleanup and minor tweaks

--- src/commands/Fun/Random/poll.ts
+++ src/commands/Fun/Random/poll.ts
@@ -9,7 +9,7 @@ export default class extends SteveCommand {
 		super(store, file, directory, {
 			description: `Have ${NAME} create a poll for you`,
 			extendedHelp: 'This command requires you to have at least two options and no more than ten.',
-			examples: ['poll Who is best?|ench|alys'],
+			examples: ['poll Is butt legs?|Butt is legs|Butt is butt'],
 			usage: '<question:string> <option1:string> <option2:string> [...]',
 			helpUsage: 'question | option 1 | option 2 | ...'
 		});
@@ -18,18 +18,18 @@ export default class extends SteveCommand {
 	public async run(msg: KlasaMessage, choices: string[]): Promise<Message> {
 		if(choices.length>11)
 			return msg.channel.send(`You have too many choices! The max is 10!`);
-		const emotes = ['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🔟'];
 		
-		let message = choices[0] +`\n`;
+			const emotes = ['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🔟'];
+		
+		let out = `**`+ choices[0] + `**`+`\n`;
 		for(var i = 1; i < choices.length; i++)
-			message +=emotes[i-1] + ` ` + choices[i] +`\n`;
-		msg.channel.send(message).then(function(message){
+			out +=emotes[i-1] + ` ` + choices[i] +`\n`;
+		
+		msg.channel.send(out).then(function(message){
 			for(var i = 0; i < choices.length - 1; i++)
 				message.react(emotes[i]);
 		});
 
-		//for(var i = 0; i < choices.length; i++)
-		//	out.react(emotes[i]);
 		return ;
 	}
 
