342023ad8fcdfc79c3595795822f59aae72316b9
Jonathan <jonathanwford156@gmail.com>
2020-10-03 11:05:25 -0400

    update lyrics command

--- src/commands/Miscellaneous/lyrics.ts
+++ src/commands/Miscellaneous/lyrics.ts
@@ -1,4 +1,4 @@
-import genius from 'genius-lyrics';
+import * as Genius from 'genius-lyrics';
 import { CommandStore, KlasaMessage } from 'klasa';
 import { SteveCommand } from '@lib/structures/commands/SteveCommand';
 import { Message, MessageEmbed } from 'discord.js';
@@ -16,11 +16,10 @@ export default class extends SteveCommand {
 	}
 
 	public async run(msg: KlasaMessage, [song]: [string]): Promise<Message> {
-		const Genius = new genius.Client(TOKENS.GENIUS);
-		const search = await Genius.findTrack(song);
-		const { hits } = search.response;
+		const Client = new Genius.Client(TOKENS.GENIUS);
+		const songs = await Client.songs.search(song);
 
-		if (!hits || hits.length < 1) throw msg.language.tget('COMMAND_LYRICS_NOLYRICS');
+		if (songs.length < 1) throw msg.language.tget('COMMAND_LYRICS_NOLYRICS');
 
 		const EMBED_DATA = msg.language.tget('COMMAND_LYRICS_EMBED');
 
@@ -28,10 +27,10 @@ export default class extends SteveCommand {
 			.setTitle(EMBED_DATA.TITLE);
 
 		for (let i = 0; i < 5; i++) {
-			if (!hits[i]) break;
+			if (!songs[i]) break;
 			embed
 				.addFields([
-					{ name: hits[i].result.full_title, value: hits[i].result.url }
+					{ name: songs[i].fullTitle, value: songs[i].url }
 				]);
 		}
 
