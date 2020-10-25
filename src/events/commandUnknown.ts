4d68aef702720a7aa289803373ba240bc1280431
Jonathan <jonathanwford156@gmail.com>
2020-10-25 09:14:19 -0400

    add custom command syntax for assignable roles

--- src/events/commandUnknown.ts
+++ src/events/commandUnknown.ts
@@ -3,6 +3,7 @@ import { GuildSettings } from '@lib/types/settings/GuildSettings';
 import { Snippet } from '../commands/Snippets/snippet';
 import { SteveCommand } from '@lib/structures/commands/SteveCommand';
 import { floatPromise } from '@utils/util';
+import { Role } from 'discord.js';
 
 export default class extends Event {
 
@@ -11,9 +12,37 @@ export default class extends Event {
 
 		cmd = cmd.toLowerCase();
 
+		const roles
+			= msg.guild!.roles.cache.filter(r => (msg.guild!.settings.get(GuildSettings.Roles.Assignable) as string[]).includes(r.id))
+				.map(r => r.name.toLowerCase());
+
+		const role = roles.some(r => r === cmd);
+		if (role) return this.runAssign(msg, msg.guild!.roles.cache.find(r => r.name.toLowerCase() === cmd)!);
+
+
 		const snips: Snippet[] = msg.guild.settings.get(GuildSettings.Snippets);
 		const snip = snips.some(s => s.name === cmd);
 		if (snip) return this.runSnippet(msg, cmd);
+	}
+
+	private async runAssign(msg: KlasaMessage, role: Role): Promise<void> {
+		const assignCommand = this.client.commands.get('assign') as AssignCommand;
+		const timer = new Stopwatch();
+
+		try {
+			await this.client.inhibitors.run(msg, assignCommand);
+
+			try {
+				const commandRun = assignCommand.run(msg, [role]);
+				timer.stop();
+				const response = await commandRun;
+				floatPromise(this, this.client.finalizers.run(msg, assignCommand, response, timer));
+			} catch (err) {
+				this.client.console.error(err);
+			}
+		} catch (response) {
+			this.client.emit('commandInhibited', msg, assignCommand, response);
+		}
 	}
 
 	private async runSnippet(msg: KlasaMessage, cmd: string): Promise<void> {
@@ -36,6 +65,10 @@ export default class extends Event {
 		}
 	}
 
+}
+
+interface AssignCommand extends SteveCommand {
+	run(msg: KlasaMessage, roles: Role[]): Promise<KlasaMessage>;
 }
 
 interface SnippetCommand extends SteveCommand {
