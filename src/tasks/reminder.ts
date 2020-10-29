c1db891695e67636a3c91a94b6c1075945c3fdd0
Jonathan <jonathanwford156@gmail.com>
2020-10-29 18:17:13 -0400

    fix(reminders): reminders not tagging users

--- src/tasks/reminder.ts
+++ src/tasks/reminder.ts
@@ -9,7 +9,7 @@ export default class extends Task {
 		// @ts-expect-error 2339
 		const _channel = this.client.channels.cache.get(data.channelID ?? data.channel);
 		// @ts-expect-error 2339
-		const _user = this.client.users.cache.get(data.userID ?? data.user);
+		const _user = await this.client.users.fetch(data.userID ?? data.user);
 
 		if (_channel && _channel.isText()) return _channel.send(`${_user}, here's the reminder you asked for: **${data.content}**`);
 	}
