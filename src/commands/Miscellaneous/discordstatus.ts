e2f0518f7c65f5c6b2379bac4af74fe2048a45ad
BenSegal855 <bensegal@udel.edu>
2020-10-31 17:55:01 -0400

    linting

--- src/commands/Miscellaneous/discordstatus.ts
+++ src/commands/Miscellaneous/discordstatus.ts
@@ -2,7 +2,7 @@ import { SteveCommand } from '@lib/structures/commands/SteveCommand';
 import { CommandStore, KlasaMessage } from 'klasa';
 import { Message, MessageEmbed } from 'discord.js';
 import { formatDate } from '@utils/util';
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 export default class extends SteveCommand {
 
@@ -18,20 +18,20 @@ export default class extends SteveCommand {
 	public async run(msg: KlasaMessage): Promise<Message> {
 		const url = 'https://srhpyqt94yxb.statuspage.io/api/v2/summary.json';
 
-		return fetch(url, { method: "Get" })
+		return fetch(url, { method: 'Get' })
 			.then(res => res.json())
-			.then((json) => {
+			.then(json => {
 				const embed = new MessageEmbed()
-				.setTitle(json.status.description)
-				.setDescription(msg.language.tget('COMMAND_DISCORD_STAUTS_EMBED_DECRIPTION', json.status.indicator))
-				.addFields(json.components.map((component: { name: any; status: any; }) => ({
-					name: component.name,
-					value: component.status,
-					inline: true
-				})))
-				.setFooter(msg.language.tget('COMMAND_DISCORD_STATUS_EMBED_FOOTER', formatDate(json.page.updated_at)));
+					.setTitle(json.status.description)
+					.setDescription(msg.language.tget('COMMAND_DISCORD_STAUTS_EMBED_DECRIPTION', json.status.indicator))
+					.addFields(json.components.map((component: { name: any; status: any }) => ({
+						name: component.name,
+						value: component.status,
+						inline: true
+					})))
+					.setFooter(msg.language.tget('COMMAND_DISCORD_STATUS_EMBED_FOOTER', formatDate(json.page.updated_at)));
 
-			return msg.channel.send(embed);
+				return msg.channel.send(embed);
 			});
 	}
 
