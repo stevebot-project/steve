6e6d4c18e4d8842b4e26f1ceb83e1c9f87273c5c
Jonathan <jonathanwford156@gmail.com>
2020-12-16 18:11:30 -0500

    audino and f slash commands

--- src/events/raw.ts
+++ src/events/raw.ts
@@ -12,11 +12,27 @@ export default class extends Event {
 		const guild = this.client.guilds.cache.get(data.guild_id) ?? null;
 		const channel = guild ? guild.channels.cache.get(data.channel_id) : null;
 
+		if (data.data.name === 'audino' && channel) floatPromise(this, this.audinoSlash(guild!, channel));
 		if (data.data.name === 'dftba' && channel) floatPromise(this, this.dftbaSlash(guild!, channel));
+		if (data.data.name === 'pressf' && channel) floatPromise(this, this.pressfSlash(guild!, channel));
+	}
+
+	private async audinoSlash(guild: Guild, channel: GuildChannel) {
+		if (channel.isText()) {
+			return channel.send(guild.language.tget('COMMAND_AUDINO_ID'),
+				{ files: [{ attachment: './assets/images/john_screech.png', name: 'john_screech.png' }] });
+		}
 	}
 
 	private async dftbaSlash(guild: Guild, channel: GuildChannel) {
 		if (channel.isText()) return channel.send(guild.language.randomDftba);
+	}
+
+	private async pressfSlash(guild: Guild, channel: GuildChannel) {
+		if (channel.isText()) {
+			return channel.send(guild.language.tget('COMMAND_F_ID'),
+				{ files: [{ attachment: './assets/images/f.png', name: 'pay_respects.png' }] });
+		}
 	}
 
 }
