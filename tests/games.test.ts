09e675dd9f0400d8299daf8493090bf43402212e
Jonathan <jonathanwford156@gmail.com>
2021-01-27 17:46:24 -0500

    tests(rps): add tests for rps's checkWinner function

---
+++ tests/games.test.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+import { checkWinner } from '../src/lib/util/RockPaperScissors';
+
+describe('RockPaperScissors', () => {
+	describe('checkWinner', () => {
+		test('GIVEN draw THEN return 0', () => {
+			const expectedResult = 0;
+
+			expect(checkWinner('rock', 'rock')).toBe(expectedResult);
+			expect(checkWinner('paper', 'paper')).toBe(expectedResult);
+			expect(checkWinner('scissors', 'scissors')).toBe(expectedResult);
+		});
+
+		test('GIVEN bot win THEN return -1', () => {
+			const expectedResult = -1;
+
+			expect(checkWinner('rock', 'scissors')).toBe(expectedResult);
+			expect(checkWinner('paper', 'rock')).toBe(expectedResult);
+			expect(checkWinner('scissors', 'paper')).toBe(expectedResult);
+		});
+
+		test('GIVEN player win THEN return 1', () => {
+			const expectedResult = 1;
+
+			expect(checkWinner('rock', 'paper')).toBe(expectedResult);
+			expect(checkWinner('paper', 'scissors')).toBe(expectedResult);
+			expect(checkWinner('scissors', 'rock')).toBe(expectedResult);
+		});
+	});
+});
