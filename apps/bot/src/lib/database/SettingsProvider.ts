917c637db77b8d9bfc692e10a7f84cbdd8549b11
jwford <jonathanwford156@gmail.com>
2026-04-15 00:45:23 -0400

    eslint logistics

--- apps/bot/src/lib/database/SettingsProvider.ts
+++ apps/bot/src/lib/database/SettingsProvider.ts
@@ -3,14 +3,14 @@ import FeedbackSettings from "./FeedbackSettings.js";
 import GuildSettings from "./GuildSettings.js";
 
 export default class SettingsProvider {
-  public feedback: FeedbackSettings;
-  public guilds: GuildSettings;
+	public feedback: FeedbackSettings;
+	public guilds: GuildSettings;
 
-  private prisma: PrismaClient;
+	private prisma: PrismaClient;
 
-  public constructor() {
-    this.prisma = new PrismaClient();
-    this.feedback = new FeedbackSettings(this.prisma);
-    this.guilds = new GuildSettings(this.prisma);
-  }
+	public constructor() {
+		this.prisma = new PrismaClient();
+		this.feedback = new FeedbackSettings(this.prisma);
+		this.guilds = new GuildSettings(this.prisma);
+	}
 }
