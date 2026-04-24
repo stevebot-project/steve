35a242cbd323567dceebc7508dd887bc3e102797
jwford <jonathanwford156@gmail.com>
2026-04-23 22:45:15 -0400

    feat(snips): add methods for working with snippet settings

--- apps/bot/src/lib/database/SettingsProvider.ts
+++ apps/bot/src/lib/database/SettingsProvider.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@steve/database";
 import FeedbackSettings from "./FeedbackSettings.js";
 import GuildSettings from "./GuildSettings.js";
+import SnippetSettings from "./SnippetSettings.js";
 
 export default class SettingsProvider {
 	public feedback: FeedbackSettings;
 	public guilds: GuildSettings;
+	public snippets: SnippetSettings;
 
 	private prisma: PrismaClient;
 
@@ -12,5 +14,6 @@ export default class SettingsProvider {
 		this.prisma = new PrismaClient();
 		this.feedback = new FeedbackSettings(this.prisma);
 		this.guilds = new GuildSettings(this.prisma);
+		this.snippets = new SnippetSettings(this.prisma);
 	}
 }
---
+++ apps/bot/src/lib/database/SnippetSettings.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@steve/database";
+import { Snowflake } from "discord.js";
+
+export default class SnippetSettings {
+	private prisma: PrismaClient;
+
+	public constructor(prisma: PrismaClient) {
+		this.prisma = prisma;
+	}
+
+	public async deleteSnippet(guildId: Snowflake, name: string) {
+		return this.prisma.snippet.delete({
+			where: { guildId_name: { guildId, name } },
+		});
+	}
+
+	public async getGuildSnippets(guildId: Snowflake) {
+		return this.prisma.snippet.findMany({
+			where: { guildId },
+		});
+	}
+
+	public async getSnippet(guildId: Snowflake, name: string) {
+		return this.prisma.snippet.findUnique({
+			where: { guildId_name: { guildId, name } },
+		});
+	}
+
+	// prisma upsert lets us use one method for creating and updating snippets
+	public async updateSnippet(
+		guildId: Snowflake,
+		name: string,
+		content: string,
+		embed: boolean,
+	) {
+		return this.prisma.snippet.upsert({
+			where: { guildId_name: { guildId, name } },
+			create: { guildId, name, content, embed },
+			update: { name, content, embed },
+		});
+	}
+}
