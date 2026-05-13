963ff29c931dcbfd3776733b450333777f216e88
jwford <jonathanwford156@gmail.com>
2026-05-12 20:33:15 -0400

    feat(roll): add wilko's roll command, modified for slash commands rather than message commands

--- apps/bot/src/lib/i18n/keys/commands/Fun.ts
+++ apps/bot/src/lib/i18n/keys/commands/Fun.ts
@@ -1,4 +1,6 @@
-import { T } from "#utils/i18n";
+import { FT, T } from "#utils/i18n";
 
 export const AudinoAlt = T("commands/fun:audino_alt");
 export const FAlt = T("commands/fun:f_alt");
+export const RollResponse = T("commands/fun:roll_response");
+export const RollEmojiResponse = FT<{ emoji: string; result: string }>("commands/fun:roll_emoji_response");
