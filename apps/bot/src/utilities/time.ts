abf41c4cbf1aa9c7e69e7638da81fc6717ebbffc
jwford <jonathanwford156@gmail.com>
2026-05-23 14:06:59 -0400

    chore(util): update parse duration utility to be arrow function

--- apps/bot/src/utilities/time.ts
+++ apps/bot/src/utilities/time.ts
@@ -3,17 +3,17 @@ import { Duration } from "@sapphire/duration";
 import { Utility } from "@sapphire/plugin-utilities-store";
 import { Timestamp } from "@sapphire/timestamp";
 
-@ApplyOptions<Utility.Options>({
-	name: "time",
-})
+@ApplyOptions<Utility.Options>({ name: "time" })
 export class TimeUtility extends Utility {
 	public defaultDateFormat = new Timestamp("YYYY MMM D");
 
-	public parseDuration(input: string) {
+	public parseDuration = (input: string) => {
 		const duration = new Duration(input);
 
 		return duration ? duration.offset : null;
-	}
+	};
+
+
 }
 
 declare module "@sapphire/plugin-utilities-store" {
